c0c154bf9554fa3b7c6816a766162d1d0a6de491
Federico <fedecor@protonmail.com>
2024-05-29 00:18:42 +0200

    last-edit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -168,13 +168,13 @@ const triggerScene = async (device) => {
   @apply col-span-6 pr-0 sm:flex sm:pr-0;
 }
 .el-form-item{
-  @apply grid grid-cols-8
+  @apply grid grid-cols-8;
 }
 .el-button{
-  @apply col-span-3
+  @apply col-span-3;
 }
 .el-form-item__label{
-  @apply col-span-2 justify-start
+  @apply col-span-2 justify-start;
 }
 .bottone{
   @apply flex justify-center;
@@ -184,42 +184,7 @@ const triggerScene = async (device) => {
 }
 </style>
 <style scoped>
-/*** iPhone and iOS Form Input Zoom Fixes ***/
-/* Fix Input Zoom on devices older than iPhone 5: */
-@media screen and (device-aspect-ratio: 2/3) {
-    select, textarea, input[type="text"], input[type="password"],
-    input[type="datetime"], input[type="datetime-local"],
-    input[type="date"], input[type="month"], input[type="time"],
-    input[type="week"], input[type="number"], input[type="email"],
-    input[type="url"]{ font-size: 16px; }
-}
 
-/* Fix Input Zoom on iPhone 5, 5C, 5S, iPod Touch 5g */
-@media screen and (device-aspect-ratio: 40/71) {
-    select, textarea, input[type="text"], input[type="password"],
-    input[type="datetime"], input[type="datetime-local"],
-    input[type="date"], input[type="month"], input[type="time"],
-    input[type="week"], input[type="number"], input[type="email"],
-    input[type="url"]{ font-size: 16px; }
-}
-
-/* Fix Input Zoom on iPhone 6, iPhone 6s, iPhone 7  */
-@media screen and (device-aspect-ratio: 375/667) {
-    select, textarea, input[type="text"], input[type="password"],
-    input[type="datetime"], input[type="datetime-local"],
-    input[type="date"], input[type="month"], input[type="time"],
-    input[type="week"], input[type="number"], input[type="email"], 
-    input[type="tel"], input[type="url"]{ font-size: 16px; }
-}
-
-/* Fix Input Zoom on iPhone 6 Plus, iPhone 6s Plus, iPhone 7 Plus, iPhone 8, iPhone X, XS, XS Max  */
-@media screen and (device-aspect-ratio: 9/16) {
-    select, textarea, input[type="text"], input[type="password"],
-    input[type="datetime"], input[type="datetime-local"],
-    input[type="date"], input[type="month"], input[type="time"],
-    input[type="week"], input[type="number"], input[type="email"],
-    input[type="tel"], input[type="url"]{ font-size: 16px; }
-}
 #nav {
   margin: 0 auto;
 
